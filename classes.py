8f98144d0fa1bc9b6107f5ff6462775eff8d24d8
arjun356 <sarang.svkm@gmail.com>
2019-02-08 06:08:01 +0530

    classesprogram

---
+++ classes.py
@@ -0,0 +1,12 @@
+class students:
+    name=""
+    age=""
+
+    def display (self,x,y):
+        students.name=x
+        students.age=y
+        print(students.name)
+        print(students.age)
+
+s=students()
+s.display("sarang",18)
